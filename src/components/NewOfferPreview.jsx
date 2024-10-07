eb56f40754ce7bac9c16f49d428aaad2e5cf6621
Victor Carreno <vmanuelcar@hotmail.com>
2024-10-07 13:54:33 -0500

    🔥 HOTFIX - Add optional chaining fix to data usage at NewOfferPreview

--- src/components/NewOfferPreview.jsx
+++ src/components/NewOfferPreview.jsx
@@ -2,24 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NewOfferPreview = ({ data }) => {
-    const dataCurrentPrice = data.priceInfo.currentPrice?.priceString;
-    const dataPriceRange = data.priceInfo.priceRange?.priceString;
+    const dataCurrentPrice = data.priceInfo?.currentPrice?.priceString;
+    const dataPriceRange = data.priceInfo?.priceRange?.priceString;
     const dataAverageRating = String(data.averageRating);
 
     return (
         <article className='max-w-[220px] w-[35vw] h-min rounded-xl text-clamp-sm'>
             <Link to={`/product/${data.usItemId}`}>
-                <img className='max-w-[220px] w-[35vw] max-h-[220px] h-[35vw] border-gray-300 border-4 rounded-xl' src={data.imageInfo.thumbnailUrl} alt={data.name} />
+                <img className='max-w-[220px] w-[35vw] max-h-[220px] h-[35vw] border-gray-300 border-4 rounded-xl' src={data.imageInfo?.thumbnailUrl} alt={data.name} />
                 {dataPriceRange && (
                     <p className='h-[5vw] max-h-8 mt-[2%]'>
-                      <span className='font-medium'>{dataPriceRange || dataCurrentPrice}</span>
-                      <span className='mx-1 font-medium'>{data.currencyCode}</span>
+                        <span className='font-medium'>{dataPriceRange || dataCurrentPrice}</span>
+                        <span className='mx-1 font-medium'>{data.currencyCode}</span>
                     </p>
                 )}
                 {!dataPriceRange && (
                     <p className='h-[5vw] max-h-8 mt-[2%]'>
-                      <span className='font-medium'>{data.currencyCode}</span>
-                      <span className='mx-1 font-medium'>{dataCurrentPrice}</span>
+                        <span className='font-medium'>{data.currencyCode}</span>
+                        <span className='mx-1 font-medium'>{dataCurrentPrice}</span>
                     </p>
                 )}
 
